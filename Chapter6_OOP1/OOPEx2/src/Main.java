6b439c844783c17ccbd8163f9127fe90163b6a48
choiyoung69 <choiyoung609@gmail.com>
2024-03-14 12:40:00 +0900

    Chapter 6 study

---
+++ Chapter6_OOP1/OOPEx2/src/Main.java
@@ -0,0 +1,11 @@
+class Tv{
+
+}
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Tv[] t = new Tv[3];
+        System.out.println(t[0]);
+    }
+}
---
+++ Chapter6_OOP1/OOPEx4/src/Main.java
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,2,1,6,5,4};
+
+        printArr(arr);
+        sortArr(arr);
+        printArr(arr);
+        System.out.println("sum = " + sumArr(arr));
+    }
+
+    static void printArr(int[] arr){
+        System.out.print("[");
+
+        for(int i : arr){
+            System.out.print(i + ", ");
+        }
+        System.out.println("]");
+    }
+    static int sumArr(int[] arr){
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+    static void sortArr(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 0; j < arr.length - 1 - i; i++){
+                if(arr[j] > arr[j + 1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
